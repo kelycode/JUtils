d7eceb35da630eb5a6421e1377d55d1e65daeade
kavin <2522582332@qq.com>
2021-12-22 08:19:14 +0800

    Add files via upload
    增加文件

---
+++ com/kavin/jutils/utils/CleanUtil.java
@@ -0,0 +1,79 @@
+package com.kavin.jutils.utils;
+
+import android.os.Environment;
+
+import com.kavin.jutils.utils.io.FileUtil;
+
+import java.io.File;
+
+/**
+ * 清除相关工具类
+ */
+public final class CleanUtil {
+
+    private CleanUtil() {
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    /**
+     * 清除内部缓存
+     * <p>/data/data/com.xxx.xxx/cache</p>
+     *
+     * @return {@code true}: 清除成功<br>{@code false}: 清除失败
+     */
+    public static boolean cleanInternalCache() {
+        return FileUtil.getInstance().delete(JUtils.getApp().getCacheDir());
+    }
+
+    /**
+     * 清除内部文件
+     * <p>/data/data/com.xxx.xxx/files</p>
+     *
+     * @return {@code true}: 清除成功<br>{@code false}: 清除失败
+     */
+    public static boolean cleanInternalFiles() {
+        return FileUtil.getInstance().delete(JUtils.getApp().getFilesDir());
+    }
+
+    /**
+     * 清除内部数据库
+     * <p>/data/data/com.xxx.xxx/databases</p>
+     *
+     * @return {@code true}: 清除成功<br>{@code false}: 清除失败
+     */
+    public static boolean cleanInternalDbs() {
+        return FileUtil.getInstance().delete(JUtils.getApp().getFilesDir().getParent() + File.separator + "databases");
+    }
+
+    /**
+     * 根据名称清除数据库
+     * <p>/data/data/com.xxx.xxx/databases/dbName</p>
+     *
+     * @param dbName 数据库名称
+     * @return {@code true}: 清除成功<br>{@code false}: 清除失败
+     */
+    public static boolean cleanInternalDbByName(String dbName) {
+        return JUtils.getApp().deleteDatabase(dbName);
+    }
+
+    /**
+     * 清除内部 SP
+     * <p>/data/data/com.xxx.xxx/shared_prefs</p>
+     *
+     * @return {@code true}: 清除成功<br>{@code false}: 清除失败
+     */
+    public static boolean cleanInternalSP() {
+        return FileUtil.getInstance().delete(JUtils.getApp().getFilesDir().getParent() + File.separator + "shared_prefs");
+    }
+
+    /**
+     * 清除外部缓存
+     * <p>/storage/emulated/0/android/data/com.xxx.xxx/cache</p>
+     *
+     * @return {@code true}: 清除成功<br>{@code false}: 清除失败
+     */
+    public static boolean cleanExternalCache() {
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                && FileUtil.getInstance().delete(JUtils.getApp().getExternalCacheDir());
+    }
+}
